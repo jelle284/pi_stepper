ff5f70882bd1542648686fe7079390e675c37079
Jesper Seegert <33733782+jelle284@users.noreply.github.com>
2023-12-30 22:41:19 +0100

    Update move.py

--- move.py
+++ move.py
@@ -26,19 +26,19 @@ import motor
 
 def main(args):
     print(args)
-    if len(args) > 0:
+    if len(args) > 1:
         pos = float(args[1])
     else:
         pos = 33
-    if len(args) > 1:
+    if len(args) > 2:
         vel = float(args[2])
     else:
         vel = 10
-    if len(args) > 2:
+    if len(args) > 3:
         accel = float(args[3])
     else:
         accel = 20
-    if len(args) > 3:
+    if len(args) > 4:
         jerk = float(args[4])
     else:
         jerk = 60
