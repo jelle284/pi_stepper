f8a9dcb6c3d189fcbfd6d71c703f7a5b0ab607fe
Jesper Seegert <33733782+jelle284@users.noreply.github.com>
2024-06-20 22:51:49 +0200

    Update rem.py

--- rem.py
+++ rem.py
@@ -9,15 +9,22 @@ moves = ((1<<21, 200, 600),
 pi = pigpio.pi()
 assert(pi.connected == 1)
 pi.wave_clear()
-
-for pin, nsteps, delayus in moves:
-    wf = [pigpio.pulse(pin, 0, int(delayus/2)), pigpio.pulse(0, pin, int(delayus/2))]*nsteps
-    pi.wave_add_generic(wf)
-    
-wid = pi.wave_create()
-pi.wave_send_once(wid)
-
-while pi.wave_tx_busy():
-    time.sleep(0.1)
+first = True
+wold = -1
+for i in range(56):
+    for pin, nsteps, delayus in moves:
+        wf = [pigpio.pulse(pin, 0, int(delayus/2)), pigpio.pulse(0, pin, int(delayus/2))]*nsteps
+        pi.wave_add_generic(wf)
+    wid = pi.wave_create_and_pad(25)
+    if first:
+        pi.wave_send_once(wid)
+        wold = wid
+        first = False
+        continue
+    pi.wave_send_using_mode(pigpio.WAVE_MODE_ONE_SHOT_SYNC)
+    while pi.wave_tx_at == wold:
+        time.sleep(0.01)
+    pi.wave_delete(wold)
+    wold=wid
     
 pi.stop()
